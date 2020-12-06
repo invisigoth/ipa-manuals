e742fe5a9003f7665ae9248028cbdfe3444ffdf2
Jason Guo <jason@xing.com.au>
2020-12-07 10:22:23 +1100

    remove config ignore and add ds

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit048c54664ae8adbc79630f281aee50ed
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit048c54664ae8adbc79630f281aee50ed', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit048c54664ae8adbc79630f281aee50ed', 'loadClassLoader'));
@@ -34,7 +32,7 @@ class ComposerAutoloaderInit048c54664ae8adbc79630f281aee50ed
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit048c54664ae8adbc79630f281aee50ed::getInitializer($loader));
         } else {
